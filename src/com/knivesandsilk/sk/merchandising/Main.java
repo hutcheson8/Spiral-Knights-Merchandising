abeb994aaf9288421a32249778a3120258fc4942
Wesley Hutcheson <hutcheson8@gmail.com>
2023-09-02 16:02:50 -0500

    Add input persistence and change reserves display

--- src/com/knivesandsilk/sk/merchandising/Main.java
+++ src/com/knivesandsilk/sk/merchandising/Main.java
@@ -1,5 +1,7 @@
 package com.knivesandsilk.sk.merchandising;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
 import javafx.scene.input.Clipboard;
@@ -16,19 +18,29 @@ import java.awt.event.WindowListener;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.PrintStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main implements Serializable {
+  private static final long serialVersionUID = -5633752954235124061L;
+  private static final Gson GSON = new Gson();
   private static final String LEDGER_FILE_NAME = "ledger.srl";
+  private static final String DAILY_INPUTS_FILE_NAME = "inputs.json";
   private final static MouseListener COPIER = new MouseListener() {
     @Override
     public void mouseClicked(MouseEvent e) {
@@ -46,9 +58,7 @@ public class Main implements Serializable {
     public void mousePressed(MouseEvent e) {
       HashMap<DataFormat, Object> data = new HashMap<>();
       data.put(DataFormat.PLAIN_TEXT, ((JLabel) e.getComponent()).getText());
-      Platform.runLater(() -> {
-        Clipboard.getSystemClipboard().setContent(data);
-      });
+      Platform.runLater(() -> Clipboard.getSystemClipboard().setContent(data));
     }
 
     @Override
@@ -61,20 +71,56 @@ public class Main implements Serializable {
    */
   private final static String[] DAILY_COLUMNS =
       {"Expired Items", "Leftover Items", "Active Listings", "AH Price", "AH Items/Listing"};
+  private static List<String> INPUTS;
   private final ArrayList<Item> items = new ArrayList<>();
   private Float energyPrice;
 
+  //  public static ObjectInputStream getSwapOIS(InputStream in, String fromClass, String toClass) throws IOException {
+  //    final String from = "^" + fromClass, fromArray = "^\\[L" + fromClass, toArray = "[L" + toClass;
+  //    return new ObjectInputStream(in) {
+  //      protected Class<?> resolveClass(ObjectStreamClass desc) throws ClassNotFoundException {
+  //        String name = desc.getName().replaceFirst(from, toClass);
+  //        name = name.replaceFirst(fromArray, toArray);
+  //        return Class.forName(name);
+  //      }
+  //
+  //      protected ObjectStreamClass readClassDescriptor() throws IOException, ClassNotFoundException {
+  //        ObjectStreamClass cd = super.readClassDescriptor();
+  //        String name = cd.getName().replaceFirst(from, toClass);
+  //        name = name.replaceFirst(fromArray, toArray);
+  //        if(!name.equals(cd.getName())) {
+  //          cd = ObjectStreamClass.lookup(Class.forName(name));
+  //        }
+  //        return cd;
+  //      }
+  //    };
+  //  }
+
   public static void main(String[] args) {
     Main main;
-    try {
-      ObjectInputStream loadingStream =
-          new ObjectInputStream(new BufferedInputStream(new FileInputStream(LEDGER_FILE_NAME)));
+    try(ObjectInputStream loadingStream = new ObjectInputStream(
+        new BufferedInputStream(new FileInputStream(LEDGER_FILE_NAME)))) {
       main = (Main) loadingStream.readObject();
-      loadingStream.close();
     } catch(Exception e) {
+      e.printStackTrace();
       main = new Main();
     }
     main.run();
+  }
+
+  private static void saveInputs() {
+    try(PrintStream inputsPrintStream = new PrintStream(
+        new BufferedOutputStream(new FileOutputStream(DAILY_INPUTS_FILE_NAME)))) {
+      inputsPrintStream.println(GSON.toJson(INPUTS));
+    } catch(FileNotFoundException ignored) {
+    }
+  }
+
+  private static void loadInputs() {
+    try(Scanner inputsScanner = new Scanner(new BufferedInputStream(new FileInputStream(DAILY_INPUTS_FILE_NAME)))) {
+      INPUTS = GSON.fromJson(inputsScanner.nextLine(), TypeToken.getParameterized(List.class, String.class).getType());
+    } catch(FileNotFoundException ignored) {
+    }
   }
 
   private void constructDaily(Runnable update) {
@@ -90,8 +136,8 @@ public class Main implements Serializable {
       daily.add(itemName);
       //KNIVES Resume here
       for(int y = 0; y < DAILY_COLUMNS.length; y++) {
-        textFields[x * DAILY_COLUMNS.length + y] = new JTextField();
-        JTextField textField = textFields[x * DAILY_COLUMNS.length + y];
+        JTextField textField = new JTextField();
+        textFields[x * DAILY_COLUMNS.length + y] = textField;
         daily.add(textField);
       }
     }
@@ -105,12 +151,14 @@ public class Main implements Serializable {
     JTextField energyReserves = new JTextField();
     textFields[textFields.length - 1] = energyReserves;
     daily.add(energyReserves);
+    Optional<Iterator<String>> inputIterator = Optional.ofNullable(INPUTS).map(Collection::iterator);
+    inputIterator.ifPresent(input -> Arrays.stream(textFields).forEach(textField -> textField.setText(input.next())));
     JButton submit = new JButton("Submit");
     submit.addActionListener((e) -> {
       try {
         record(textFields);
         update.run();
-        daily.setVisible(false);
+        daily.dispose();
         constructSellPrompt();
       } catch(Exception f) {
         if(f instanceof GoodDealException) {
@@ -124,7 +172,7 @@ public class Main implements Serializable {
     });
     daily.add(submit);
     JButton cancel = new JButton("Cancel");
-    cancel.addActionListener((e) -> daily.setVisible(false));
+    cancel.addActionListener((e) -> daily.dispose());
     daily.add(cancel);
     // for (int x = 0; x < DAILY_COLUMNS.length - ?; x++) {
     // daily.add(new JPanel());
@@ -133,10 +181,40 @@ public class Main implements Serializable {
     daily.pack();
     daily.setLocationRelativeTo(null);
     daily.setResizable(false);
+    daily.addWindowListener(new WindowListener() {
+      @Override
+      public void windowOpened(WindowEvent e) {
+      }
+
+      @Override
+      public void windowClosing(WindowEvent e) {
+      }
+
+      @Override
+      public void windowClosed(WindowEvent e) {
+      }
+
+      @Override
+      public void windowIconified(WindowEvent e) {
+      }
+
+      @Override
+      public void windowDeiconified(WindowEvent e) {
+      }
+
+      @Override
+      public void windowActivated(WindowEvent e) {
+      }
+
+      @Override
+      public void windowDeactivated(WindowEvent e) {
+        INPUTS = Arrays.stream(textFields).map(JTextField::getText).collect(Collectors.toList());
+      }
+    });
     daily.setVisible(true);
   }
 
-  private final void constructSellPrompt() {
+  private void constructSellPrompt() {
     JFrame sellPrompt = new JFrame("Sell");
     sellPrompt.setLayout(new GridLayout(0, 3));
     JLabel name = new JLabel("Name"), numItems = new JLabel("# Items"), price = new JLabel("Price");
@@ -192,22 +270,21 @@ public class Main implements Serializable {
     sellPrompt.setVisible(true);
   }
 
-  private final void constructSpendingOffers() {
+  private void constructSpendingOffers() {
     JFrame spendingOffers = new JFrame("Spending/Offers");
     spendingOffers.setLayout(new GridLayout(0, 3));
-    int crownOffers = 0, energyOffers = 0;
+    int crownReserves = 0, energyReserves = 0;
     for(Item i : items) {
-      crownOffers += i.getRequiredCrownReserves();
-      energyOffers += i.getRequiredEnergyReserves();
+      crownReserves += i.getRequiredCrownReserves();
+      energyReserves += i.getRequiredEnergyReserves();
     }
-    crownOffers = crownOffers / 5000 + 1;
-    energyOffers = energyOffers / 100 + 1;
+    crownReserves += 25000;//For Basil recipes
     spendingOffers.add(new JLabel("Operation"));
     spendingOffers.add(new JLabel("Crowns"));
     spendingOffers.add(new JLabel("Energy"));
-    spendingOffers.add(new JLabel("Offers: "));
-    spendingOffers.add(new JLabel(crownOffers + "x5000 Offers"));
-    spendingOffers.add(new JLabel(energyOffers + "x100 Offers"));
+    spendingOffers.add(new JLabel("Reserves: "));
+    spendingOffers.add(new JLabel(crownReserves + " CR"));
+    spendingOffers.add(new JLabel(energyReserves + " CE"));
     spendingOffers.add(new JLabel("Remainder: "));
     JTextField crownRemain = new JTextField();
     spendingOffers.add(crownRemain);
@@ -279,18 +356,18 @@ public class Main implements Serializable {
     spendingOffers.setVisible(true);
   }
 
-  private final void finish() {
+  private void finish() {
     switch(JOptionPane.showConfirmDialog(null, "Save Your Work?")) {
       case 0:
-        try {
-          ObjectOutputStream savingStream =
-              new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(LEDGER_FILE_NAME)));
+        INPUTS = null;
+        try(ObjectOutputStream savingStream = new ObjectOutputStream(
+            new BufferedOutputStream(new FileOutputStream(LEDGER_FILE_NAME)))) {
           savingStream.writeObject(this);
-          savingStream.close();
         } catch(Exception e) {
           e.printStackTrace();
         }
       case 1:
+        saveInputs();
         System.exit(0);
         break;
       default:
@@ -306,7 +383,7 @@ public class Main implements Serializable {
     }
   }
 
-  private final void record(JTextField[] data) throws Exception {
+  private void record(JTextField[] data) throws Exception {
     energyPrice = ((float) Integer.parseInt(data[data.length - 2].getText())) / 100;
     int energyReserves = Integer.parseInt(data[data.length - 1].getText());
     List<RecordParams> recordParamsList = new ArrayList<>(items.size());
@@ -341,6 +418,7 @@ public class Main implements Serializable {
   }
 
   private void run() {
+    loadInputs();
     new JFXPanel();
     JFrame reminder = new JFrame("Reminder");
     History history = new History();
@@ -490,7 +568,7 @@ public class Main implements Serializable {
             case 6:
               return "Price";
             case 7:
-              return "Cost Plus %";
+              return "Cost Plus Ratio";
             case 8:
               return "Undercut Margin";
             default:
